8d5256dab93acdc78e4d7358ee95356d62298f1f
Ростислав <lishuk.rostislaw@yandex.ru>
2021-01-14 19:14:24 +0800

    Pre-Alpha

--- F5/settings.py
+++ F5/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'F5.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
