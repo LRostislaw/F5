e138461df53c2343bb7187001f63f86e42370a43
Ростислав <lishuk.rostislaw@yandex.ru>
2021-01-14 20:40:31 +0800

    Pre-Alpha

--- F5/settings.py
+++ F5/settings.py
@@ -118,17 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = ''
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'main/staticfiles')
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'main/media')
 
 LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = ''
