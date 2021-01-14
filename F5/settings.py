7834b219b025971bcc6a994c03f9992a74d20812
Ростислав <lishuk.rostislaw@yandex.ru>
2021-01-14 17:53:30 +0800

    Pre-Alpha

--- F5/settings.py
+++ F5/settings.py
@@ -116,12 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'F5/main/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    (os.path.join(BASE_DIR, 'static')),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = ''
